ac2c91077dd8edbd3d5a15531a0d2b73a9573259
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-22 16:20:50 +0200

    Started on Message, might not be done.

--- src/network/Message.java
+++ src/network/Message.java
@@ -1,5 +1,32 @@
 package network;
 
-public class Message {
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
 
+
+
+public class Message {
+	private int timeToLive;
+	private ShortestPath pathHome;
+	private Hashtable visitedNodes;
+	public Message(){
+		visitedNodes = new Hashtable(4099);
+	}
+	public int getTimeToLive(){
+		return timeToLive;
+	}
+	public ShortestPath getPathHome(){
+		return pathHome;
+	}
+	public Hashtable getVisitedNodes(){
+		return visitedNodes;
+	}
+	public void setTimeToLive(int x){
+		timeToLive = x;
+	}
+	public void setPathHome(ShortestPath sp){
+		pathHome = sp;
+	}
+	public void setVisitedNodes(Hashtable table){
+		visitedNodes = table;
+	}
 }
