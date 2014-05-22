c13440cdf0fe89053fbbd88cf5fd7169d39fbcb8
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-22 16:46:29 +0200

    Did event, might not be done

--- src/network/Event.java
+++ src/network/Event.java
@@ -1,5 +1,22 @@
 package network;
 
 public class Event {
-
+	private int uniqueID;
+	private Position homePos;
+	private int dateOfBirth;
+	
+	public Event(int id, Position pos, int dob){
+		uniqueID = id;
+		homePos = pos;
+		dateOfBirth = dob;
+	}
+	public int getID(){
+		return uniqueID;
+	}
+	public int getDateOfBirth(){
+		return dateOfBirth;
+	}
+	public Position getHomePos(){
+		return homePos;
+	}
 }
