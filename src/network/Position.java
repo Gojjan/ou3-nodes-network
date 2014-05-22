4dd004b8ea58af83828616673ca63306e1b0023b
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-22 15:04:59 +0200

    Did Position

--- src/network/Position.java
+++ src/network/Position.java
@@ -1,5 +1,24 @@
 package network;
 
 public class Position {
-
+	private int x;
+	private int y;
+	
+	public Position(int nx, int ny){
+		x = nx;
+		y = ny;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public boolean equals(Position pos){
+		if(pos.getX() == x && pos.getY() == y){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
