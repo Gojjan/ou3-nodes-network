c884b8d30134bf51b7b6dfee29e5bbcba116c94d
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-22 15:44:22 +0200

    Started ShortestPath

--- src/network/ShortestPath.java
+++ src/network/ShortestPath.java
@@ -1,5 +1,28 @@
 package network;
 
-public class ShortestPath {
+import java.util.Stack;
 
+public class ShortestPath {
+	private Stack<Position> positionStack;
+	private int distance;
+	
+	public ShortestPath(Position pos){
+		positionStack = new Stack<Position>();
+		positionStack.push(pos);
+	}
+	public void addDirection(Position pos){
+		positionStack.push(pos);
+	}
+	public Position getNextDirection(){
+		return positionStack.peek();
+	}
+	public void popNextDirection(){
+		positionStack.pop();
+	}
+	public int getDistance(){
+		return distance;
+	}
+	public boolean compareDistance(ShortestPath sp){
+		return true;
+	}
 }
