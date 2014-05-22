f921e5a52e1a0425a2a8fe64b3b6f8c9c88dd0d8
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-22 16:41:32 +0200

    Started working on Request, might not be done.

--- src/network/Request.java
+++ src/network/Request.java
@@ -1,5 +1,10 @@
 package network;
 
-public class Request {
-
+public class Request extends Message{
+	private int targetID;
+	public Request(int eventID, int timeToLive, Position pos){
+		targetID = eventID;
+		this.setTimeToLive(timeToLive);
+		this.setPathHome(new ShortestPath(pos));
+	}
 }
