9c3c5f1e5077d3db923b188f12e53b583712c958
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-23 14:51:11 +0200

    gav node alla viktiga attributer och funktioner

--- src/network/Node.java
+++ src/network/Node.java
@@ -1,10 +1,22 @@
 package network;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+
 public class Node {
+	private Hashtable eventTable;
+	private Hashtable sendRequests;
+	private boolean isRepeater;
+	private boolean isHoldingMessage;
+	private Position pos;
+	private ArrayList neighbours;
 	private Queue<QueuedMessage> sendQueue;
 	private Position position;
+	public Node(Position npos){
+		
+	}
 	public void receiveMessage(Object o){
 		if(o instanceof Agent){
 			Agent agent = (Agent)o;
@@ -16,5 +28,20 @@ public class Node {
 		} else if (o instanceof Response){
 			
 		}
+	}
+	public void timeTick(){
+		
+	}
+	public void sendMessage(QueuedMessage qdm){
+		
+	}
+	public boolean getIsHoldingMessage(){
+		return isHoldingMessage;
+	}
+	public Position getPosition(){
+		return pos;
+	}
+	public void setRepeater(){
+		
 	}
 }
