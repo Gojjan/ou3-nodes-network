8a1971ef3ed305ff22bcf4986da3e75ac418e828
Alex <Alex@Alex-Dator>
2014-05-26 23:20:31 +0200

    queued message sparar data från parameter objektet

--- src/network/QueuedMessage.java
+++ src/network/QueuedMessage.java
@@ -9,20 +9,33 @@ public class QueuedMessage extends Message{
 	private int timeToLive;
 	private Hashtable visitedNodes;
 	private Hashtable eventTable;
+	private ArrayList<Integer> definedKeys = new ArrayList<Integer>();
 	private Event foundEvent;
+	private int targetId;
+	private boolean isOnTrack;
 	public QueuedMessage(Object o, Position pos){
 		if(o instanceof Agent){
 			type = 1;
 			Agent agent = (Agent)o;
-			
+			definedKeys = agent.getDefinedKeys();
+			eventTable = agent.getEventTable();
 			
 		} else if (o instanceof Request){
 			type = 2;
 			Request request = (Request)o;
+			timeToLive = request.getTimeToLive();
+			pathHome = request.getPathHome();
+			visitedNodes = request.getVisitedNodes();
+			targetId = request.getTargetId();
+			isOnTrack = request.getIsOnTrack();
 			
 		} else if (o instanceof Response){
 			type = 3;
 			Response response = (Response)o;
+			timeToLive = response.getTimeToLive();
+			pathHome = response.getPathHome();
+			visitedNodes = response.getVisitedNodes();
+			foundEvent = response.getEvent();
 		}
 	}
 	public int getType(){
