50e4be25076bbaf5fb44e799c24a1c6a0a81db11
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-26 16:52:32 +0200

    Ändrade visitedNode-relatedade metoder.

--- src/network/Agent.java
+++ src/network/Agent.java
@@ -5,23 +5,31 @@ import java.util.ArrayList;
 import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
 
 public class Agent extends Message{
-	private Hashtable eventTable;
+	private Hashtable eventTable = new Hashtable();
 	private ArrayList<Integer> definedKeys = new ArrayList<Integer>();
 	
 	public Agent(Event event, int x, Position pos){
-		
+		ShortestPath sp = new ShortestPath(pos);
+		eventTable.put(event.getID(), sp);
+		this.setTimeToLive(x);
 	}
 	public Hashtable getEventTable(){
 		return eventTable;
 	}
-	public Boolean visitedNode(Position pos){
-		return false;
+	public void visitNode(Position pos){
+		this.visitNodeI(pos);
 	}
-	public void setDefinedKeys(ArrayList al){
+	public boolean visitedNode(Position pos){
+		return this.visitedNodeI(pos);
+	}
+	public void setDefinedKeys(ArrayList<Integer> al){
 		definedKeys = al;
 	}
-	public ArrayList getDefinedKeys(){
+	public ArrayList<Integer> getDefinedKeys(){
 		return definedKeys;
+	}
+	public void setEventTable(Hashtable hs){
+		eventTable = hs;
 	}
 
 }
