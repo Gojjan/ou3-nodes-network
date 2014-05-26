a9c16541042d1eb5d6010dc5daa8cd7e6e3b7c0c
Henrik Sjöström <draedoc@gmail.com>
2014-05-26 20:28:29 +0200

    javadoc till Request.

--- src/network/Request.java
+++ src/network/Request.java
@@ -1,19 +1,46 @@
 package network;
 
+/** Klassen Request är ett barn till {@link Message}. Det är ett meddelande som 
+ * efterfrågar ett event med en viss identifikation. Den bygger på rumor-routing 
+ * algoritmen.
+ * 
+ * @author Henrik Sjöström
+ * @version 1.0 Maj 26 2014
+ */
 public class Request extends Message{
+	/** Identifikationen på {@link Event} objektet som söks. */
 	private int targetID;
+	/** */
 	private boolean isOnTrack;
+	
+	/** Skapar ett Request.
+	 * 
+	 * @param eventID						sökta identifikationen
+	 * @param timeToLive					tidssteg kvar
+	 * @param pos							{@link Position} där den skapas
+	 */
 	public Request(int eventID, int timeToLive, Position pos){
 		targetID = eventID;
 		this.setTimeToLive(timeToLive);
 		this.setPathHome(new ShortestPath(pos));
 	}
+	/** Returnerar den söka identifikationen
+	 * 
+	 * @return								sökta identifikaionen
+	 */
 	public int getTargetId(){
 		return targetID;
 	}
+	/** */
 	public boolean getIsOnTrack(){
 		return isOnTrack;
 	}
+	/** Lägg till {@link Position} till vägen tillbaka till Requestets 
+	 * skapelseposition.
+	 * 
+	 * @param pos							{@link Position} objekt som läggs till
+	 * @see Position
+	 */
 	public void addPosToPathHome(Position pos){
 		ShortestPath sp = this.getPathHome();
 		sp.addDirection(pos);
