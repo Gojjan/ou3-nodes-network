31ba41cf56ef3d007cf78e4a45ee1f3dd1900af9
Viktor Emil Bengtsson <servo_32@hotmail.com>
2014-05-27 14:15:58 +0200

    debugging

--- src/network/Network.java
+++ src/network/Network.java
@@ -83,6 +83,7 @@ public class Network {
 	 */
 	public Node GetNodeAtPosition(Position pos){
 		System.out.println(pos.getX());
+		System.out.println(pos.getY());
 		return nodeArray.get(pos.getX()).get(pos.getY());
 	}
 	
