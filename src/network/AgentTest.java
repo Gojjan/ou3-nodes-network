02bf5b7f31d89766d977c3cea21114e17762a5f6
Henrik Sjöström <draedoc@gmail.com>
2014-08-19 14:01:29 +0200

    unit test för agent. Behöver arbete

--- src/network/AgentTest.java
+++ src/network/AgentTest.java
@@ -3,10 +3,45 @@
  */
 package network;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * @author H
  *
  */
 public class AgentTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+
+	@Test
+	public void messageTest() {
+		Agent a = new Agent(new Event(1,new Position(2,3),4),5,new Position(6,7));
+		
+		assertEquals(a.getTimeToLive(),5);
+		a.setTimeToLive(8);
+		assertEquals(a.getTimeToLive(),8);
+		
+		assertFalse(a.visitedNode(new Position(1,2)));
+		a.visitNode(new Position(1,2));
+		assertTrue(a.visitedNode(new Position(1,2)));
+	}
+	
+	@Test
+	public void eventTableTest() {
+		Agent a = new Agent(new Event(1,new Position(2,3),4),5,new Position(6,7));
+		
+		assertFalse(a.getEventTable().isEmpty());
+		assertFalse(a.getDefinedKeys().isEmpty());
+		
+		assertTrue(a.getDefinedKeys().size()==1);
+		
+		a.setDefinedKeys(2);
+		
+		assertTrue(a.getDefinedKeys().size()==2);
+	}
 
 }
