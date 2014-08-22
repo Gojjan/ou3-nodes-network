3034957bfc0b431d38b433c861a9f4b73b63dc33
H <draedoc@gmail.com>
2014-08-22 11:30:28 +0200

    Kommentarer tillaggda.

--- src/network/AgentTest.java
+++ src/network/AgentTest.java
@@ -7,16 +7,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * @author H
+/** AgentTest är ett unit-test för klassen Agent.
+ * 
+ * @author Henrik Sjöström
+ * @version 1.0 Aug 22 2014
  *
  */
 public class AgentTest {
 
-	/**
-	 * @throws java.lang.Exception
+	/** messageTest.
+	 * 
+	 * Testar om Agent's setters och getters adopterade från klassen Message 
+	 * fungerar korrekt. Detta utförs genom att först getta begynnelse värdet,
+	 * sedan setta ett nytt värde och sist getta det nya, settade värdet.
 	 */
-
 	@Test
 	public void messageTest() {
 		Agent a = new Agent(new Event(1,new Position(2,3),4),5,new Position(6,7));
@@ -30,6 +34,11 @@ public class AgentTest {
 		assertTrue(a.visitedNode(new Position(1,2)));
 	}
 	
+	/** eventTableTest.
+	 * 
+	 * Testar om Agent's eventTable börjar tomt (vilket den ej ska) och om
+	 * definedKeys växer i rätt takt.
+	 */
 	@Test
 	public void eventTableTest() {
 		Agent a = new Agent(new Event(1,new Position(2,3),4),5,new Position(6,7));
