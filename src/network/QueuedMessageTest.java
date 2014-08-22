81b7c217b9d39c7474fbd7c5d3b86b103e5fcbb1
H <draedoc@gmail.com>
2014-08-22 18:53:39 +0200

    kommentarer. Fortsätter skriva kommentarer rästen av kvällen. Går genom klasserna med.

--- src/network/QueuedMessageTest.java
+++ src/network/QueuedMessageTest.java
@@ -1,18 +1,21 @@
-/**
- * 
- */
 package network;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * @author H
+/** QueuedMessageTest är ett unit-test för klassen QueuesMessage.
+ * 
+ * @author Henrik Sjöström
+ * @version 1.0 Aug 22 2014
  *
  */
 public class QueuedMessageTest {
 
+	/** testAgent. 
+	 * 
+	 * Testar om QueuedMessage som innehåller en Agent returnerar rätt värden.
+	 */
 	@Test
 	public void testAgent() {
 		QueuedMessage q = new QueuedMessage(new Agent(new Event(1,new Position(2,3),4),5,new Position(6,7)),new Position(8,9));
@@ -23,6 +26,10 @@ public class QueuedMessageTest {
 		assertTrue(q.getDestination().equals(new Position(8,9)));
 	}
 
+	/** testRequest. 
+	 * 
+	 * Testar om QueuedMessage som innehåller en Request returnerar rätt värden.
+	 */
 	@Test
 	public void testRequest() {
 		QueuedMessage q = new QueuedMessage(new Request(1,2,new Position(3,4)),new Position(5,6));
@@ -33,6 +40,10 @@ public class QueuedMessageTest {
 		assertTrue(q.getDestination().equals(new Position(5,6)));
 	}
 	
+	/** testResponse. 
+	 * 
+	 * Testar om QueuedMessage som innehåller en Response returnerar rätt värden.
+	 */
 	@Test
 	public void testResponse() {
 		QueuedMessage q = new QueuedMessage(new Response(new ShortestPath(new Position(1,2)),new Event(3,new Position(4,5),6),new Position(7,8));
