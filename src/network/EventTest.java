dc529889812b97ba75e0c99289e0c106a0ad526b
H <draedoc@gmail.com>
2014-08-22 13:27:24 +0200

    Har nu kommentarer.

--- src/network/EventTest.java
+++ src/network/EventTest.java
@@ -8,8 +8,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/** EventTest är ett unit-test för klassen Event.
+ * 
+ * @author Henrik Sjöström
+ * @version 1.0 Aug 22 2014
+ *
+ */
 public class EventTest {
 
+	/** dateTest. 
+	 * 
+	 * Testar om Event's dateOfBirth getters och setters fungerar.
+	 */
 	@Test
 	public void dateTest() {
 		Event e = new Event(1,new Position(2,3),4);
@@ -18,14 +28,22 @@ public class EventTest {
 		assertEquals(e.getDateOfBirth(),5);
 	}
 
+	/** positionTest. 
+	 * 
+	 * Testar om Event's homePos getter och setter fungerar.
+	 */
 	@Test
-	public void PositionTest() {
+	public void positionTest() {
 		Event e = new Event(1,new Position(2,3),4);
 		assertTrue(e.getHomePos().equals(new Position(2,3)));
 		e.setHomePos(new Position(6,7));
 		assertTrue(e.getHomePos().equals(new Position(6,7)));
 	}
 	
+	/** IDTest. 
+	 * 
+	 * Testar om Event's ID getter och setter fungerar.
+	 */
 	@Test
 	public void IDTest() {
 		Event e = new Event(1,new Position(2,3),4);
